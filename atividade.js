44ef5ccc004ae80a8092722dae46ba2c030f40d9
GABRIEL XAVIER DE CASTRO FRASCO <gabriel_frasco@estudante.sesisenai.org.br>
2026-09-21 20:19:58 -0300

    top6

--- atividade.js
+++ atividade.js
@@ -1,4 +1,13 @@
-const item = "espadaDeFogo";
+const item = "ESPADA DE FOGO";
 const valorI = 2;
 const raridade = "Lendário";
 const quantidade = 10;
+
+console.log(`VOCÊ DESEJA COMPRAR
+    =====ITEM=====
+    Nome:${item}
+    Preço:${valorI}
+    Raridade:${raridade}
+    Quantidade:${quantidade}
+    ===============
+    `);
